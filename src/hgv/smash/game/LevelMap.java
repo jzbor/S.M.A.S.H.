19970562d8c1247068c7f28b006b1ae1cd694b1a
jzbor <jzbor@posteo.net>
2019-05-17 19:33:51 +0200

    Anfang LevelMap

--- src/hgv/smash/game/LevelMap.java
+++ src/hgv/smash/game/LevelMap.java
@@ -1,4 +1,10 @@
 package hgv.smash.game;
 
 public class LevelMap extends GameObject {
+
+    public static final String[] MAP_NAMES = {"Map 1", "Map 2"};
+
+    public static LevelMap load(String name) {
+        return null;
+    }
 }
