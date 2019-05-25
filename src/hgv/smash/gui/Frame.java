9eda3478b08e40a8aa7d3f820b9b67f1905f75b9
fabio <fabio@fam-neugebauer.de>
2019-05-25 20:42:42 +0200

    Hauptmenü add buttons

--- src/hgv/smash/gui/Frame.java
+++ src/hgv/smash/gui/Frame.java
@@ -1,16 +1,14 @@
 package hgv.smash.gui;
 
-import hgv.smash.game.LevelMap;
-import hgv.smash.resources.Avatar;
-
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
 
 public class Frame extends JFrame {
 
     private static Frame ourInstance = new Frame();
     private static final String TITLE = "S.M.A.S.H.";
+    private static final Dimension FINAL_SIZE = new Dimension(1024, 768);
+    private static final Dimension DEBUG_SIZE = new Dimension(1366, 768);
 
     private Frame() {
         super(TITLE);
@@ -19,17 +17,14 @@ public class Frame extends JFrame {
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(new Dimension(1024, 768));
-        ((JPanel) getContentPane()).setBorder(BorderFactory.createLineBorder(Color.RED)); // debugging purposes
 
-        try {
-            Avatar testAvatar1 = Avatar.debugAvatar();
-            Avatar testAvatar2 = Avatar.debugAvatar();
-            LevelMap levelMap = LevelMap.debugMap();
-            setContentPane(new GamePanel(testAvatar1, testAvatar2, levelMap));
-        } catch (IOException e) {
-            e.printStackTrace();
-            setContentPane(new MenuPanel());
-        }
+        JPanel panel = new MenuPanel();
+        panel.setSize(getContentPane().getSize());
+        setContentPane(panel);
+
+        ((JPanel) getContentPane()).setBorder(BorderFactory.createLineBorder(Color.RED)); // debugging purposes
+        ((JPanel) getContentPane()).updateUI();
+
     }
 
     public static Frame getInstance() {
--- src/hgv/smash/gui/MenuPanel.java
+++ src/hgv/smash/gui/MenuPanel.java
@@ -7,23 +7,36 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 //Klasse für das Start- und Hauptmenü:
 
 public class MenuPanel extends Panel implements ActionListener {
-    private JComboBox player1, player2, map;
+    private JComboBox buttonPlayer1, buttonPlayer2, buttonMap;
+    private JLabel backround;
 
-    public MenuPanel (){
+    public MenuPanel () {
+        setLayout(null);
+        backround=new JLabel (new ImageIcon("./recources/HintergrundMenü/redStar.png") );
+        backround.setBounds(200,200,1024,768);
+        add(backround);
+
         //Avatar_Names ist ein statisches Feld und ein public Attribut der Klasse Avatar.
-        player1=new JComboBox(Avatar.AVATAR_NAMES);
-        player2=new JComboBox(Avatar.AVATAR_NAMES);
+        buttonPlayer1 = new JComboBox(Avatar.AVATAR_NAMES);
+        buttonPlayer2 = new JComboBox(Avatar.AVATAR_NAMES);
         //Level_Map ist ein statisches Feld und ein public Attribut der LevelMap
-        map=new JComboBox(LevelMap.MAP_NAMES);
-        player1.addActionListener(this);
-        player2.addActionListener(this);
-        map.addActionListener(this );
-        GridLayout menuLayout= new GridLayout( 4,2);
-        this.setLayout(menuLayout);
+        buttonMap = new JComboBox(LevelMap.MAP_NAMES);
+        buttonPlayer1.addActionListener(this);
+        buttonPlayer2.addActionListener(this);
+        buttonMap.addActionListener(this);
+
+        buttonPlayer1.setBounds(362,150,150,100);
+        buttonPlayer2.setBounds(362,84+100+150,150,100);
+        buttonMap.setBounds(362,84+100+84+100+150,150,100);
+        add(buttonPlayer1);
+        add(buttonPlayer2);
+        add (buttonMap);
+
 
     }
     @Override
