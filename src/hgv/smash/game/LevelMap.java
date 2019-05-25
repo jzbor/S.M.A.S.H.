16221661ae82052bf0f7ee62a2fb0a357b9ef2ab
jzbor <jzbor@posteo.net>
2019-05-25 16:57:07 +0200

    Anfang GamePanel (incl bg)

--- src/hgv/smash/game/LevelMap.java
+++ src/hgv/smash/game/LevelMap.java
@@ -1,13 +1,29 @@
 package hgv.smash.game;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class LevelMap extends GameObject {
 
-    public static final String[] MAP_NAMES = {"Map 1", "Map 2"};
+    public static final String[] MAP_NAMES = {"map_1", "map_2"};
+    private static final String MAP_PATH = "./resources/maps/";
+    private BufferedImage backgroundImage;
 
-    public static LevelMap load(String name) {
-        return null;
+    public LevelMap(BufferedImage backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
+
+    public static LevelMap debugMap() throws IOException {
+        return load(MAP_PATH + MAP_NAMES[0]);
+    }
+
+    public static LevelMap load(String name) throws IOException {
+        File bgFile = new File(name + "-bg.jpeg");
+        BufferedImage bufferedImage = ImageIO.read(bgFile);
+        return new LevelMap(bufferedImage);
     }
 
     @Override
@@ -22,7 +38,7 @@ public class LevelMap extends GameObject {
 
     @Override
     public void draw(Graphics2D graphics2D) {
-
+        graphics2D.drawImage(backgroundImage, 0, 0, null);
     }
 
 }
--- src/hgv/smash/resources/Avatar.java
+++ src/hgv/smash/resources/Avatar.java
@@ -4,8 +4,12 @@ public class Avatar {
     public static final String[] AVATAR_NAMES = {"Avatar1", "Avatar2"};
     private static final String AVATAR_PATH = "./resources/avatars/";
 
+    public static Avatar debugAvatar() {
+        return load(null);
+    }
+
     public static Avatar load(String name) {
-        return null;
+        return new Avatar();
     }
 
 }
