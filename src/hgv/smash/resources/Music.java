2f498433a7fd73fbf6e0c276d88b4b250a57fcb9
fabio <fabio@fam-neugebauer.de>
2019-06-04 18:23:24 +0200

    Kommentare in der Menüklasse

--- src/hgv/smash/resources/Music.java
+++ src/hgv/smash/resources/Music.java
@@ -8,18 +8,32 @@ import java.io.File;
 import java.io.IOException;
 
 public class Music {
+
+    //Files von den verschiedenen Liedern
     private File fileGameMusic;
     private File fileMenuMusic;
     private File fileScoreMusic;
+
+    //File von dem Lied, dass spielt
     private File chosenMusic;
+
+    //diese drei verschiedenen Objekte können erzeugt werden und von jeder anderen Klasse aufgerufen werden
     private static Music ourInstanceGameMusic=new Music("GameMusic");
     private static Music ourInstanceMenuMusic=new Music ("MenuMusic");
     private static Music ourInstanceScoreMusic=new Music ("ScoreMusic");
+
+    //auf den Clip wird das Lied geladen und der Clip kann dann auch abgespielt werden.
     private Clip clip;
+
+    //Man kann sich entscheiden ob man ein Objekt der Klasse Musik erzeugen will, dass die Menümusik,
+    //oder die Scoremusik abspielt.
     public Music(String musicYouNeed){
+
         fileGameMusic = new File("./resources/Sounds_and_Music/Sandstorm.wav");
         fileMenuMusic=new File ("./resources/Sounds_and_Music/Jeopardy.wav");
         fileScoreMusic=new File ("./resources/Sounds_and_Music/National_Anthem_Sowjet.wav");
+
+        //hier wird entschieden welcher File dann geladen wird
         switch (musicYouNeed) {
             case "GameMusic":
                 chosenMusic=fileGameMusic;
@@ -33,6 +47,8 @@ public class Music {
             default:
                 break;
         }
+
+        //hier wird auf das Audiosystem zugegriffen und der ausgesuchte File auf den Clip geladen
         try {
             clip= AudioSystem.getClip();
             clip.open(AudioSystem.getAudioInputStream(chosenMusic));
@@ -51,9 +67,14 @@ public class Music {
     public void stop(){
         clip.stop();
     }
+
+    //Über diese Methode drei Methoden kann jede Klasse auf die drei verschiedenen singleton
+    // Objekte der Klasse Musik zugreifen.
+
     public static Music getInstanceMenuMusic() {
         return ourInstanceMenuMusic;
     }
+
     public static Music getInstanceGameMusic(){
         return ourInstanceGameMusic;
     }
