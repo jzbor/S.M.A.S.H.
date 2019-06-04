9906d7d525053db458dfc673ae29887d8aab887b
jzbor <jzbor@posteo.net>
2019-06-04 16:34:52 +0200

    scoreboard looks ok now

--- src/hgv/smash/gui/ScorePanel.java
+++ src/hgv/smash/gui/ScorePanel.java
@@ -6,6 +6,8 @@ import hgv.smash.resources.Avatar;
 import hgv.smash.resources.Design;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -26,15 +28,53 @@ public class ScorePanel extends Panel implements ActionListener {
         JLabel gameoverLabel = new JLabel("Game Over");
         JLabel wIconLabel = new JLabel(new ImageIcon(winner.getAvatar().getImage(Avatar.NORMAL)));
         JLabel wNameLabel = new JLabel(winner.toString());
+        JLabel wScoreLabel = new JLabel("Winner");
         JLabel lIconLabel = new JLabel(new ImageIcon(winner.getAvatar().getImage(Avatar.NORMAL)));
+        JLabel lNameLabel = new JLabel(looser.toString());
+        JLabel lScoreLabel = new JLabel("Looser");
+
+        JPanel scorePanel = new JPanel();
+        JPanel p1Panel = new JPanel();
+        JPanel p2Panel = new JPanel();
+
 
         // Configure elements
+        Border paddingBorder = new EmptyBorder(30, 30, 30, 30);
         gameoverLabel.setFont(Design.getGameoverFont(80));
         gameoverLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
+        wIconLabel.setBorder(paddingBorder);
+        wNameLabel.setFont(Design.getDefaultFont());
+        wNameLabel.setBorder(paddingBorder);
+        wScoreLabel.setFont(Design.getDefaultFont());
+        wScoreLabel.setBorder(paddingBorder);
+        lIconLabel.setBorder(paddingBorder);
+        lNameLabel.setFont(Design.getDefaultFont());
+        lNameLabel.setBorder(paddingBorder);
+        lScoreLabel.setFont(Design.getDefaultFont());
+        lScoreLabel.setBorder(paddingBorder);
+
+        scorePanel.setLayout(new BoxLayout(scorePanel, BoxLayout.PAGE_AXIS));
+        p1Panel.setLayout(new BorderLayout());
+        p2Panel.setLayout(new BorderLayout());
+
         // Compose layout
         setLayout(new BorderLayout());
+
+        p1Panel.add(wIconLabel, BorderLayout.LINE_START);
+        p1Panel.add(wNameLabel, BorderLayout.CENTER);
+        p1Panel.add(wScoreLabel, BorderLayout.LINE_END);
+
+        p2Panel.add(lIconLabel, BorderLayout.LINE_START);
+        p2Panel.add(lNameLabel, BorderLayout.CENTER);
+        p2Panel.add(lScoreLabel, BorderLayout.LINE_END);
+
+        scorePanel.add(p1Panel);
+        scorePanel.add(p2Panel);
+
+
         add(gameoverLabel, BorderLayout.NORTH);
+        add(scorePanel, BorderLayout.CENTER);
         setSize(Frame.getInstance().getContentPane().getSize());
         setPreferredSize(getSize());
 
