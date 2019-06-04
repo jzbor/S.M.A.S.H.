093e5e76a7d689d6f775f12623ac77217b9ff783
jzbor <jzbor@posteo.net>
2019-06-04 13:58:38 +0200

    Added fonts to menu panel and improved 'Design' class

--- src/hgv/smash/resources/Design.java
+++ src/hgv/smash/resources/Design.java
@@ -10,7 +10,7 @@ public class Design {
     private static final String SECONDARY_COLOR = "#000000";
     private static final String FONT_DIR = "./resources/fonts/";
     private static final String TITLE_FONT = "ZCOOL_KuaiLe/ZCOOLKuaiLe-Regular.ttf";
-    private static final String DEFAULT_FONT = "ZCOOL_KuaiLe/ZCOOLKuaiLe-Regular.ttf";
+    private static final String DEFAULT_FONT = "Russo_One/RussoOne-Regular.ttf";
 
     public static Color getPrimaryColor() {
         return Color.decode(PRIMARY_COLOR);
@@ -20,64 +20,38 @@ public class Design {
         return Color.decode(SECONDARY_COLOR);
     }
 
-    public static Font getTitleFont() {
+    private static Font getFont(String path, int type, float size) {
         try {
-            File file = new File(FONT_DIR + TITLE_FONT);
-            return Font.createFont(Font.TRUETYPE_FONT, file).deriveFont(Font.BOLD, 24);
+            File file = new File(path);
+            return Font.createFont(Font.TRUETYPE_FONT, file).deriveFont(type, size);
         } catch (FontFormatException | IOException e) {
             e.printStackTrace();
             return Font.getFont(Font.SANS_SERIF);
         }
+    }
+
+    public static Font getTitleFont() {
+        return getFont(FONT_DIR + TITLE_FONT, Font.BOLD, 24);
     }
 
     public static Font getTitleFont(float size) {
-        try {
-            File file = new File(FONT_DIR + TITLE_FONT);
-            return Font.createFont(Font.TRUETYPE_FONT, file).deriveFont(Font.BOLD, size);
-        } catch (FontFormatException | IOException e) {
-            e.printStackTrace();
-            return Font.getFont(Font.SANS_SERIF);
-        }
+        return getFont(FONT_DIR + TITLE_FONT, Font.BOLD, size);
     }
 
     public static Font getTitleFont(int type, float size) {
-        try {
-            File file = new File(FONT_DIR + TITLE_FONT);
-            return Font.createFont(Font.TRUETYPE_FONT, file).deriveFont(type, size);
-        } catch (FontFormatException | IOException e) {
-            e.printStackTrace();
-            return Font.getFont(Font.SANS_SERIF);
-        }
+        return getFont(FONT_DIR + TITLE_FONT, type, size);
     }
 
     public static Font getDefaultFont() {
-        try {
-            File file = new File(FONT_DIR + DEFAULT_FONT);
-            return Font.createFont(Font.TRUETYPE_FONT, file).deriveFont(Font.BOLD, 24);
-        } catch (FontFormatException | IOException e) {
-            e.printStackTrace();
-            return Font.getFont(Font.SANS_SERIF);
-        }
+        return getFont(FONT_DIR + DEFAULT_FONT, Font.PLAIN, 18);
     }
 
     public static Font getDefaultFont(float size) {
-        try {
-            File file = new File(FONT_DIR + DEFAULT_FONT);
-            return Font.createFont(Font.TRUETYPE_FONT, file).deriveFont(Font.BOLD, size);
-        } catch (FontFormatException | IOException e) {
-            e.printStackTrace();
-            return Font.getFont(Font.SANS_SERIF);
-        }
+        return getFont(FONT_DIR + DEFAULT_FONT, Font.PLAIN, size);
     }
 
     public static Font getDefaultFont(int type, float size) {
-        try {
-            File file = new File(FONT_DIR + DEFAULT_FONT);
-            return Font.createFont(Font.TRUETYPE_FONT, file).deriveFont(type, size);
-        } catch (FontFormatException | IOException e) {
-            e.printStackTrace();
-            return Font.getFont(Font.SANS_SERIF);
-        }
+        return getFont(FONT_DIR + DEFAULT_FONT, type, size);
     }
 
 }
